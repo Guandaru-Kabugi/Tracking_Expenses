0ab41f23c76ce65b499ee1dd78c386fe2fa99766
Guandaru-Kabugi <kabugi96@gmail.com>
2025-01-10 10:32:09 +0300

    added filtering of the tags

--- expenses/filters.py
+++ expenses/filters.py
@@ -1,5 +1,6 @@
 import django_filters
 from .models import Item
+from taggit.models import Tag
 #here, I learned that we can implement a custom filter using django_filters.filterset
 #I am filtering through numbers, which is why I have Numberfilter
 class ItemFilter(django_filters.FilterSet):
@@ -8,7 +9,18 @@ class ItemFilter(django_filters.FilterSet):
     item_category = django_filters.NumberFilter(field_name='item_category', lookup_expr='exact')  # Case-insensitive exact match
     date_recorded = django_filters.DateFilter(field_name='date_recorded',lookup_expr='icontains')
     cost = django_filters.NumberFilter(field_name='cost',lookup_expr='icontains')
+    tags = django_filters.CharFilter(method='filter_by_tags')
 
     class Meta:
         model = Item
-        fields = ['item_name', 'item_category','date_recorded','cost']
+        fields = ['item_name', 'item_category','date_recorded','cost','tags']
+
+    def filter_by_tags(self, queryset, name, value):
+        """
+        Custom filter to search by tags using a comma-separated list.
+        """
+        # Split the input value into individual tags
+        tag_list = [tag.strip() for tag in value.split(',') if tag.strip()]
+
+        # Filter the queryset to match items with any of the given tags
+        return queryset.filter(tags__name__in=tag_list).distinct()
